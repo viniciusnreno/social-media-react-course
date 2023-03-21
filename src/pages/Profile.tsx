dcbf7d71260053c6426849c5af75670285063e38
Vinicius Reno <mineiro.reno@gmail.com>
2023-03-20 23:40:10 -0300

    profile img

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -3,9 +3,11 @@ import { useAuthState } from 'react-firebase-hooks/auth'
 import { auth } from '../config/firebase'
 const Profile = () => {
   const [user] = useAuthState(auth)
+  console.log(user?.photoURL)
   return (
     <div>
       <h1>{user?.displayName}</h1>
+      <img className="mx-auto" src={user?.photoURL || ""} alt="" />
     </div>
   )
 }
