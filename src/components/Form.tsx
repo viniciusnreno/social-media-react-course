301bab6fd405520a7e454e4de361942060152086
Vinicius Reno <mineiro.reno@gmail.com>
2023-03-21 22:06:55 -0300

    firebase deploy

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -37,7 +37,7 @@ const Form = () => {
   } 
 
   return (
-    <form onSubmit={handleSubmit(userSubmit)} className="w-1/3 mx-auto text-left">
+    <form onSubmit={handleSubmit(userSubmit)} className="md:w-1/3 w-full px-3 mx-auto text-left">
       <div className="mb-4">
         <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
         <input {...register("title")} type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
